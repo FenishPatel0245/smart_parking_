df073a795596f3cb2de2445deb6802083b237224
FenishPatel0245 <patelfenish245@gmail.com>
2025-12-08 17:10:55 -0500

    Update datetime offset issue

--- SmartParkingLot.Domain/Models/ActivityLogEntry.cs
+++ SmartParkingLot.Domain/Models/ActivityLogEntry.cs
@@ -5,6 +5,7 @@ namespace SmartParkingLot.Domain.Models;
 public class ActivityLogEntry
 {
     public Guid Id { get; set; } = Guid.NewGuid();
+    // TODO: Update datetime offset issue
     public DateTime Timestamp { get; set; } = DateTime.Now;
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
