70c53e37eb9000cc693be4da7707ef60216f9204
FenishPatel0245 <patelfenish245@gmail.com>
2025-12-07 20:04:08 -0500

    Remove validation pipeline behavior

--- SmartParkingLot.Web/Program.cs
+++ SmartParkingLot.Web/Program.cs
@@ -1,5 +1,6 @@
 // TODO: Clean up unused namespaces
 // TODO: Fix NuGet packages (consolidate versions, remove unused, resolve vulnerabilities)
+// TODO: Remove validation pipeline behavior
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using Microsoft.Extensions.DependencyInjection;
